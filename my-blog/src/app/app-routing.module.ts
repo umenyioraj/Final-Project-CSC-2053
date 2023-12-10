6a529bd725c330236af643fc02d516b1ae48a2bc
umenyioraj <129884687+umenyioraj@users.noreply.github.com>
2023-12-10 14:37:37 -0500

    edits

--- my-blog/src/app/app-routing.module.ts
+++ my-blog/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule,Routes } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+
 
 export const routes: Routes = [
 { path: 'profile', component: ProfileComponent },
-{path: 'app', component: AppComponent}];
+{path: 'app', component: AppComponent}, {path: 'users', component: UsersComponent}];
 
 
 @NgModule({
--- my-blog/src/app/app.module.ts
+++ my-blog/src/app/app.module.ts
@@ -4,11 +4,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module'; // Import the routing module
 import { ProfileComponent } from './profile/profile.component';
+import { UsersComponent } from './users/users.component';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
-  declarations: [AppComponent, ProfileComponent],
+  declarations: [AppComponent, ProfileComponent, UsersComponent],
   imports: [BrowserModule, AppRoutingModule], // Include the AppRoutingModule here
   bootstrap: [AppComponent],
+  
 })
 export class AppModule {}
